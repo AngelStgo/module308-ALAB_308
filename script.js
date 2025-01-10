638f98076e7e3274a924797e6cb4c76f550f4370
AngelStgo <euangel.stgo@gmail.com>
2025-01-09 20:15:05 -0500

    finished

--- script.js
+++ script.js
@@ -44,30 +44,47 @@ const numRemainder = numMulti % n4 ;
 console.log(numRemainder); // 0
 
 
+
 // === Part 2: Practical Math ===
 
 //You are planning a cross-country road trip!
 // The distance of the trip, in total, is 1,500 miles.
+// You have a fuel budget of $175.
+// The average cost of fuel is $3 per gallon.
+
 let tripDistance = 1500; //miles
+const fuelBudget = 175; 
+const fuelPerGallon = 3; 
 
 // Your car’s fuel efficiency is as follows:
 // At 55mph , you get 30 miles per gallon.
 const speed55mph = 55; //mph
-const fuelByDistance55 = 30; //m  
+const fuelByDistance55mph = 30; 
+let totalTime55mph = tripDistance / speed55mph; //time total
+let fuelNeeded55mph = tripDistance / fuelByDistance55mph; // gallons of fuel needed
+let fuelPrice55mph = fuelNeeded55mph * fuelPerGallon; // fuel cost
+console.log(`Your total time if you drive 55mph will be: ${totalTime55mph}hr`);
+console.log(`For this trip you will need ${fuelNeeded55mph} gallons of Fuel and the total cost for Fuel will be ${fuelPrice55mph} dollars.`)
 
 // At 60mph, you get 28 miles per gallon.
 const speed60mph = 60; //mph
-const fuelByDistance60 = 28;
+const fuelByDistance60mph = 28;
+let totalTime60mph = tripDistance / speed60mph; //time total
+let fuelNeeded60mph = tripDistance / fuelByDistance60mph; // gallons of fuel needed
+let fuelPrice60mph = fuelNeeded60mph * fuelPerGallon; // fuel cost
+console.log(`Your total time if you drive 60mph will be: ${totalTime60mph}hr`);
+console.log(`For this trip you will need ${fuelNeeded60mph} gallons of Fuel and the total cost for Fuel will be ${fuelPrice60mph} dollars.`)
 
 // At 75mph, you get 23 miles per gallon.
 const speed75mph = 75; //mph
-const fuelByDistance75 = 23;
+const fuelByDistance75mph = 23;
+let totalTime75mph = tripDistance / speed75mph; //time total
+let fuelNeeded75mph = tripDistance / fuelByDistance75mph; //gallons of fuel needed
+let fuelPrice75mph = fuelNeeded75mph * fuelPerGallon; // fuel cost
+console.log(`Your total time if you drive 75mph will be: ${totalTime75mph}hr`);
+console.log(`For this trip you will need ${fuelNeeded75mph} gallons of Fuel and the total cost for Fuel will be ${fuelPrice75mph} dollars.`)
 
-// You have a fuel budget of $175.
-const fuelBudget = 175; //dollars
 
-// The average cost of fuel is $3 per gallon.
-const fuelPerGallon = 3; //dollars
 
 // How many gallons of fuel will you need for the entire trip?
 // Will your budget be enough to cover the fuel expense?
